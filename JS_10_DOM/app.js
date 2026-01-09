9a5d4e0b9e17df60c9f653700088986542623459
mondalGourish <gourishmondal02@gmail.com>
2026-01-09 20:39:02 +0530

    new codes

--- JS_10_DOM/app.js
+++ JS_10_DOM/app.js
@@ -7,21 +7,46 @@
 // }
 
 //for multiple button
-let btns = document.querySelectorAll('button');
-for(btn of btns){
-    // btn.onclick = sayHello; //() do not give parenthesis, giving parenthesis means executing the function, but we are doing that it will execute once the button is clicked
-    // btn.onmouseenter = mouseHover;
-    btn.addEventListener('click',sayHello);
-    btn.addEventListener('click',sayName);
+// let btns = document.querySelectorAll('button');
+// for(btn of btns){
+//     // btn.onclick = sayHello; //() do not give parenthesis, giving parenthesis means executing the function, but we are doing that it will execute once the button is clicked
+//     // btn.onmouseenter = mouseHover;
+//     btn.addEventListener('click',sayHello);
+//     btn.addEventListener('click',sayName);
 
-}
-function sayHello(){
-    alert('Hello!');
-}
-function sayName(){
-    alert('Gourish!');
+// }
+// function sayHello(){
+//     alert('Hello!');
+// }
+// function sayName(){
+//     alert('Gourish!');
+// }
+
+// function mouseHover(){
+//     console.log('You hovered here');
+// }
+
+// let para = document.querySelector('p');
+// para.addEventListener('click', function(){
+//     console.log("Paragraph clicked");
+// });
+
+// let div = document.querySelector('.box');
+// para.addEventListener('mouseenter', function(){
+//     console.log("Mouse entered the box");
+// });
+
+let h1 = document.querySelector('h1');
+let h3 = document.querySelector('h3');
+let p = document.querySelector('p');
+let btn = document.querySelector('button');
+
+function changeColor(){
+    console.dir(this.innerText);
+    this.style.backgroundColor = "pink";
 }
 
-function mouseHover(){
-    console.log('You hovered here');
-}
+h1.addEventListener('click',changeColor);
+h3.addEventListener('click',changeColor);
+p.addEventListener('click',changeColor);
+btn.addEventListener('click',changeColor);
